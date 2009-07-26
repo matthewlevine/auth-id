5c24459ef1f2da33522120f2991190d54f2009e3
Matthew Levine <matthew@matthewlevine.com>
2009-07-26 15:45:11 -0700

    Now runs with webapp

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-print 'Content-Type: text/plain'
-print ''
-print 'Hello, world!'
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+class HomeController(webapp.RequestHandler):
+  def get(self):
+    self.response.headers['Content-Type'] = 'text/plain'
+    self.response.out.write('Home page')
+  
+
+class APIController(webapp.RequestHandler):
+  def get(self, url):
+    self.response.headers['Content-Type'] = 'text/plain'
+    self.response.out.write(url)
+
+application = webapp.WSGIApplication([
+    ('/', HomeController),
+    ('/(.+)', APIController)], debug=True)
+
+def main():
+  run_wsgi_app(application)
+
+if __name__ == "__main__":
+  main()
